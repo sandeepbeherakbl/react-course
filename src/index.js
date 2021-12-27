cc9f9eb166a78028fff3443754030bcb6e71dc41
Sandeep Kumar Behera <sandeepbeherakbl@gmail.com>
2021-12-27 21:04:05 +0530

    Webpack

---
+++ src/index.js
@@ -0,0 +1,12 @@
+function start(first,second){
+    var add = (first+second)
+    return add;
+}
+
+function end(first,second){
+    var mult = (first * second)
+    return mult;
+}
+
+console.log(start(2,4));
+console.log(start(1,2));
